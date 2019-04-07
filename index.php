7c69880fb6f13c7bad26289b385f3ccbd5669d91
Gajda <staniopl99@gmail.com>
2019-04-07 19:48:20 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Way To Fortune</title>
+</head>
+<body>
+    <h1>Way To Fortune</h1>
+    by GajdaD
+</body>
+</html>
