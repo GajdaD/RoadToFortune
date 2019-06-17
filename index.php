01761b4bc694d25089aaa097c25ebaec1a652d83
GajdaD <45543327+GajdaD@users.noreply.github.com>
2019-06-17 17:58:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,68 +8,50 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
 
 require_once 'connect.php';
 
-// Define variables and initialize with empty values
 $username = $password = "";
 $username_err = $password_err = "";
  
-// Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
  
-    // Check if username is empty
     if(empty(trim($_POST["username"]))){
         $username_err = 'Wpisz login.';
     } else{
         $username = trim($_POST["username"]);
     }
     
-    // Check if password is empty
     if(empty(trim($_POST['password']))){
         $password_err = 'Wpisz hasło.';
     } else{
         $password = trim($_POST['password']);
     }
     
-    // Validate credentials
     if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
         $sql = "SELECT id, username, password FROM users WHERE username = ?";
 
         if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_username);
 
-            // Set parameters
             $param_username = $username;
             
-            // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
-                // Store result
                 mysqli_stmt_store_result($stmt);
                 
-                // Check if username exists, if yes then verify password
                 if(mysqli_stmt_num_rows($stmt) == 1){                    
-                    // Bind result variables
                     mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
                     if(mysqli_stmt_fetch($stmt)){
                         if(password_verify($password, $hashed_password)){
-                            /* Password is correct, so start a new session and
-                            save the username to the session */
                             session_start();
                             
-                            // Store data in session variables
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
                             $_SESSION["username"] = $username;                            
                             
-                            // Redirect user to welcome page
                             header("location: game.php");
                         } else{
-                            // Display an error message if password is not valid
                             $password_err = 'Złe hasło.';
                         }
                     }
                 } else{
-                    // Display an error message if username doesn't exist
                     $username_err = 'Nie ma konta o tym loginie.';
                 }
             } else{
@@ -77,7 +59,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             }
         }
         
-        // Close statement
         mysqli_stmt_close($stmt);
     }
     
