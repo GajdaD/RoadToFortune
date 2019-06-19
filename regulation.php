7f168b6774027e26c6b0cfb04f7349b31bd78a03
Gajda <staniopl99@gmail.com>
2019-06-19 23:29:07 +0200

    Add regulation.php

---
+++ regulation.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="images/favicon.ico" />
+    <title>Way To Fortune</title>
+</head>
+<body>
+    <div id="large_view">
+        <header>
+            <div id="title_png">
+                <span style="display: inline-block;height: 100%;vertical-align: middle"></span><img src="images/title.png" alt="Error" style="vertical-align: middle;width:60%;height:70%;">
+            </div>
+        </header>
+        <div id="border_horizontal"></div>
+        <main>
+            <div id="div_login">
+                <p>REGULAMIN</p>
+                <p>Wkrótce...</p>
+            </div>
+        </main>
+    </div>
+</body>
+</html>
